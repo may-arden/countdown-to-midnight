3535c32a20fda0d67f463ebcadc2e26d8604cd70
may arden <mayard3n@gmail.com>
2020-10-22 12:12:35 -0700

    commit tree 0d7c3340e2b204acef9a8bb02ae199c699a3405e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,32 @@
 #write your code here
 
-def countdown
+# def countdown
+# end
+
+# my code which works in my terminal, & uses sleep function if I include it 
+
+def countdown(number)
+    while number > 0
+      puts "#{number} SECOND(S)!" 
+      number -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+
+# code from zayne I think 
+
+def countdown_with_sleep(time)
+    while time > 0
+        puts "#{time} SECOND(S)!"
+        sleep 1
+        time -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+
+
+
+
+
